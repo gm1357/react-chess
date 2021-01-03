1b48e136c17bab1701612cf25ad00a60ebc3fb40
Gabriel Machado <g_machado1@hotmail.com>
2021-01-03 16:32:30 -0300

    Improved and added animation to turn display

--- src/components/TurnDisplay/TurnDisplay.tsx
+++ src/components/TurnDisplay/TurnDisplay.tsx
@@ -1,9 +1,20 @@
+import { CSSTransitionGroup } from 'react-transition-group';
+
 import './TurnDisplay.css';
 
 function TurnDisplay(props: any) {
-    const whoseTurn = <span className="whose-turn">{props.isBlackTurn ? 'BLACK': 'WHITE'}</span>
+    const whoseTurn = <span key={props.isBlackTurn} className="whose-turn">{props.isBlackTurn ? 'BLACK': 'WHITE'}</span>
     return (
-        <span className="turn-display">{whoseTurn}'S TURN</span>
+        <span className="turn-display">
+            <CSSTransitionGroup
+                className="animation-turn"
+                transitionName="whose-turn"
+                transitionEnterTimeout={800}
+                transitionLeaveTimeout={800}>
+                {whoseTurn}
+            </CSSTransitionGroup>
+            <span>'S TURN</span>
+        </span>
     );
 }
 
